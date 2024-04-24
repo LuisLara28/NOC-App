29b8c17558efdd6410a9b25f97b6acd8720e8f38
LuisLara28 <luis_28_92lara@hotmail.com>
2024-04-23 19:29:07 -0600

    Architecture pattern

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,20 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repository/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+	new FileSystemDatasource()
+);
 
 export class Server {
 	public static start() {
 		console.log("Server started...");
 
 		CronService.createJob("*/5 * * * * *", () => {
-			const url = "https://google.com";
+			const url = "http://google.com";
 			new CheckService(
+				fileSystemLogRepository,
 				() => console.log(`${url} is ok`),
 				(error) => console.log(error)
 			).execute(url);
